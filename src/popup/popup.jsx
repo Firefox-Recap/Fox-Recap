fd4e1613e554fbb3171151fcbfc425d828b76774
katesawtell <125850781+katesawtell@users.noreply.github.com>
2025-03-26 23:44:25 -0700

    Feature/background videos (#19)
    * background videos work
    
    * prompts show up
    
    * fixed some prompts

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -8,69 +8,15 @@ import promptsData from './prompts.json';
 const Popup = () => {
   const [view, setView] = useState('home'); // "home" or "slides"
   const [timeRange, setTimeRange] = useState(null);
-  const [historyData, setHistoryData] = useState([]);
   const [loading, setLoading] = useState(false);
 
   const handleTimeRangeSelect = async (range) => { // Made this function async
     setLoading(true);
     setTimeRange(range);
     setView('slides');
-    await fetchHistory(range);
     setLoading(false);
   };
 
-  useEffect(() => {
-    if (view !== "home") {
-      fetchHistory(view);
-    }
-  }, [view]);
-
-  const fetchHistory = useCallback(async (range) => {
-    setLoading(true);
-    try {
-      const { historyLoading, historyData } = await browser.storage.local.get([
-        "historyLoading",
-        "historyData",
-      ]);
-
-      if (historyLoading) {
-        return;
-      }
-
-      if (!historyData || historyData.length === 0) {
-        setHistoryData([]);
-      } else {
-        const domainGroups = {};
-        historyData.forEach((item) => {
-          let domain;
-          try {
-            domain = new URL(item.url).hostname;
-          } catch (e) {
-            domain = item.url; // Fallback
-          }
-          if (!domainGroups[domain]) {
-            domainGroups[domain] = {
-              domain,
-              category: item.category || "Uncategorized",
-              visitCount: 0,
-            };
-          }
-          domainGroups[domain].visitCount++;
-        });
-
-        const sortedHistory = Object.values(domainGroups).sort(
-          (a, b) => b.visitCount - a.visitCount
-        );
-
-        setHistoryData(sortedHistory);
-
-      }
-    } catch (err) {
-      console.error("❌ Error loading history:", err);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
 
   return (
     <div className="container">
@@ -81,7 +27,6 @@ const Popup = () => {
           timeRange={timeRange} 
           setView={setView} 
           prompts={promptsData.prompts}
-          historyData={historyData}
         />
       )}
     </div>

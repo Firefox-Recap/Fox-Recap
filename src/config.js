31abdec1c411eda9ce7d69b3e84dae4aa1123fa0
tshasan <taimurhasan@proton.me>
2025-04-24 15:34:21 -0700

    Added files

---
+++ src/config.js
@@ -0,0 +1,30 @@
+// milliseconds in one day
+export const MS_PER_DAY = 24 * 60 * 60 * 1000;
+
+// default time‐ranges
+export const DAYS_MAP = {
+  day:   1,
+  week:  7,
+  month: 30,
+};
+
+// default time‐ranges for the history
+export const DEFAULT_CONCURRENCY = 5; 
+
+// The link for the blocklist
+export const OISD_BLOCKLIST_URL = 'https://big.oisd.nl/domainswild';
+
+// Map scores to labels
+export const CATEGORIES = [
+    'News',
+    'Entertainment',
+    'Shop',
+    'Chat',
+    'Education',
+    'Government',
+    'Health',
+    'Technology',
+    'Work',
+    'Travel',
+    'Uncategorized',
+  ];
